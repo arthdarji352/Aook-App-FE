f610ca77774d58a5de880710723885ed671714a4
Arth-ReactJs <arth@solulab.com>
2023-06-27 18:08:41 +0530

    Fe
    FE

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Books from "./components/Book/Books";
+import AddBook from "./components/AddBook";
+import About from "./components/About";
+import BookDetail from "./components/Book/BookDetail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <Header />
       </header>
-    </div>
+      <main>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/add" element={<AddBook />} />
+          <Route exact path="/books" element={<Books />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/books/:id" element={<BookDetail />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
